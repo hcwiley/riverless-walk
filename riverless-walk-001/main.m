aa90d1fa185aa082546d36b59582c5b33983fec3
LSU School of Art <LSU@0-1e-c2-c0-87-e0.wlan.lsu.edu>
2012-02-15 09:51:01 -0600

    Initial Commit

---
+++ riverless-walk-001/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  riverless-walk-001
+//
+//  Created by LSU School of Art on 2/15/12.
+//  Copyright (c) 2012 Digital Art. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
---
+++ riverless-walk-001Tests/riverless_walk_001Tests.m
@@ -0,0 +1,32 @@
+//
+//  riverless_walk_001Tests.m
+//  riverless-walk-001Tests
+//
+//  Created by LSU School of Art on 2/15/12.
+//  Copyright (c) 2012 Digital Art. All rights reserved.
+//
+
+#import "riverless_walk_001Tests.h"
+
+@implementation riverless_walk_001Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in riverless-walk-001Tests");
+}
+
+@end
